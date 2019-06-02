48358476bf13489410584e10cb26872b83e3c58e
Mephistopheles <deanesturco@gmail.com>
2019-06-03 04:22:12 +0800

    added functions to create user and post tables in database>migrations folder.

---
+++ database/migrations/2019_06_02_195234_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('name', 50);
+            $table->string('email', 50);
+            $table->string('username', 50);
+            $table->string('password', 50);
+            $table->binary('profile picture');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
---
+++ database/migrations/2019_06_02_195311_create_post_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->bigIncrements('postID');
+            $table->string('postTitle', 100);
+            $table->string('author', 50);
+            $table->string('password', 20);https://github.com/laurelius/webpro2finals.git
+            $table->binary('picture');
+            $table->dateTime('createdAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}

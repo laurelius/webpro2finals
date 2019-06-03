403c11d8ee2c266c11fe634c9ab229977762ada4
laurelius <rhejlaurel@gmail.com>
2019-06-03 09:55:03 +0800

    ye

--- storage/framework/views/7385c0a27157aa6489fae1d1259856b3b660b4c6.php
+++ storage/framework/views/7385c0a27157aa6489fae1d1259856b3b660b4c6.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
 
     <title><?php echo e(config('app.name', 'FGOFORUM')); ?></title>
     </head>
